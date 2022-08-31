665b4bddd5cc41dbdd401601eec9cae68b6e8e59
Sergey <strogalev.s@gmail.com>
2022-08-31 19:39:54 +0300

    add Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
